b1d1eb8a546475ef583c97d9afeefbfedf17d899
Caren678 <1011421526@qq.com>
2026-03-26 11:09:18 +0800

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,10 +1,8 @@
-// Paste your Firebase config here.
-// When all values stay empty, the website runs in local demo mode.
 export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyDcew5Dvz3MQntjx4eivlCXN92unYf2-6A",
+  authDomain: "the-world-of-paper-cuts.firebaseapp.com",
+  projectId: "the-world-of-paper-cuts",
+  storageBucket: "the-world-of-paper-cuts.firebasestorage.app",
+  messagingSenderId: "53148775163",
+  appId: "1:53148775163:web:b195347041f411c885b65e"
 };
